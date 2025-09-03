bfbecaf6f4bf7426c83a6cbdb97fc3cb0c7bbeeb
Tjeerd Wokke <tj.wokke@gmail.com>
2025-09-03 12:39:31 +0200

    home update

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -22,24 +22,100 @@ defineProps({
 <template>
 
     <Head title="Welcome" />
-
     <div>
-        <div v-if="canLogin">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')">
-            Dashboard</Link>
 
-            <template v-else>
-                <Link :href="route('login')">
-                Log in</Link>
+        <header class="navbar">
+            <div>
+                logo
+            </div>
+            <div>
+                zoeken
+            </div>
+            <div v-if="canLogin">
+                <Link v-if="$page.props.auth.user" :href="route('dashboard')">
+                Dashboard</Link>
 
-                <Link v-if="canRegister" :href="route('register')">
-                Register</Link>
-            </template>
-        </div>
+                <template v-else>
+                    <Link :href="route('login')">
+                    Log in</Link>
+
+                    <Link v-if="canRegister" :href="route('register')">
+                    Register</Link>
+
+                </template>
+            </div>
+        </header>
+
+        <body>
+
+            <main>
+<h1> I love Web</h1>
+
+                <div class="notes">
+
+                    <div class="note">
+                        <h2>Notes 1</h2>
+                        <p>Sprint 1</p>
+                        <p>2-9-25</p>
+                    </div>
+                    <div class="note">
+                        <h2>Notes 1</h2>
+                        <p>Sprint 1</p>
+                        <p>2-9-25</p>
+                    </div>
+                    <div class="note">
+                        <h2>Notes 1</h2>
+                        <p>Sprint 1</p>
+                        <p>2-9-25</p>
+                    </div>
+                    <div class="note">
+                        <h2>Notes 1</h2>
+                        <p>Sprint 1</p>
+                        <p>2-9-25</p>
+                    </div>
+                </div>
+            </main>
+
+
+
+        </body>
+
+
+
     </div>
-
-
-    <p>test</p>
 </template>
 
-<style></style>
+<style>
+.navbar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 50px;
+    background-color: darkcyan;
+    color: white;
+}
+
+main {}
+
+main h1{
+    width: fit-content;
+    margin: 0 auto 0 auto;
+    font-size: 5rem;
+}
+
+.notes {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    margin: 0 auto 0 auto;
+    width: fit-content;
+    max-width: 600px;
+    justify-content: space-between;
+}
+
+.note {
+    width: 280px;
+border-color: red;
+border-width: 1px;
+}
+</style>
