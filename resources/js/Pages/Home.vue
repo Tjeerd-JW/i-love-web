0ffb4bfc550a28c1f49087374ecdbce3cae7746f
Tjeerd Wokke <tj.wokke@gmail.com>
2025-09-03 11:37:30 +0200

    Laravel, Vue en Breeze

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+
+    <Head title="Welcome" />
+
+    <div>
+        <div v-if="canLogin">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')">
+            Dashboard</Link>
+
+            <template v-else>
+                <Link :href="route('login')">
+                Log in</Link>
+
+                <Link v-if="canRegister" :href="route('register')">
+                Register</Link>
+            </template>
+        </div>
+    </div>
+
+
+    <p>test</p>
+</template>
+
+<style></style>
