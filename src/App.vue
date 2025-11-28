fe7b0e1cc91279bb67b754ff3162d7729743df31
Tjeerd <tj.wokke@gmail.com>
2025-11-28 20:00:24 +0100

    Basic styling update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+      <!-- <router-link to="/about">About</router-link> -->
     </nav>
     <router-view />
   </div>
 </template>
 
 <style>
-/* body{
-  background-color: ;
-} */
+body {
+  background: #2a7b9b;
+  background: linear-gradient(
+      180deg,
+      rgba(42, 123, 155, 1) 0%,
+      rgba(255, 255, 255, 1) 100%
+    )
+    no-repeat;
+  background-size: cover;
+  height: 100%;
+  min-height: 100vh;
+
+  nav {
+    width: 100vw;
+    height: 40px;
+    background-color: #2a7b9b;
+    border-bottom: 2px solid white;
+    display: flex;
+    justify-content: space-around;
+
+    a {
+      color: black;
+      text-decoration: none;
+      font-size: 1.5rem;
+    }
+  }
+  main {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    width: clamp(275px, 95vw, 1000px);
+    margin: auto;
+
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <main>
     <h1>Digitale tuin</h1>
-    <p>Welkom op de homepagina!</p>
+    <p>Allemaal troep, het meeste niet perse mooi</p>
     <section>
       <h2>Verschillende pagina's met stuff</h2>
       <article>
@@ -16,7 +16,7 @@ export default {
       </article>
 
       <article>
-        <h3>LeesbareTeksten</h3>
+        <h3>Leesbare teksten</h3>
         <router-link to="/LeesbareTeksten">bekijken</router-link>
       </article>
 
@@ -29,4 +29,48 @@ export default {
 </template>
 
 <style scoped>
+main {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  width: clamp(275px, 95vw, 1000px);
+  margin: auto;
+  h1 {
+    grid-column: 2;
+    text-align: center;
+  }
+  p {
+    grid-column: 2;
+    text-align: center;
+  }
+  section {
+    grid-column: 1 / -1;
+    display: grid;
+    grid-template-columns: inherit;
+    gap: 0.5rem;
+    h2 {
+      grid-column: 1 / -1;
+      text-align: center;
+    }
+    article {
+      border: 2px solid black;
+      border-radius: 0.5rem;
+      padding: 0.5rem;
+      a {
+        transition: 0.4s ease-in-out;
+        text-decoration: none;
+        &::after {
+          content: "→";
+          opacity: 0;
+          transition: .2s ease-in-out;
+        }
+        &:hover {
+          margin-left: 0.4rem;
+          &::after {
+            opacity: 1;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
