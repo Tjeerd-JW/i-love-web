b722edaa6bbdd830bc07d3f69f57b74e65967968
Tjeerd <tj.wokke@gmail.com>
2026-01-25 12:33:16 +0100

    added evolution chain to detail page

--- src/views/PokemonDetailView.vue
+++ src/views/PokemonDetailView.vue
@@ -13,6 +13,14 @@ export default {
       pokemonCryLegacy: null,
       descriptions: [],
       currentImg: null,
+      evolutionChain: [],
+      evoStage1: {
+        name: "",
+        img: "",
+        baby: null,
+        babyItem: null,
+      },
+      evoChain: [],
     };
   },
   methods: {
@@ -24,10 +32,7 @@ export default {
     },
     async fetchPokemon(pokemonName) {
       try {
-        const response = await fetch(
-          `${pokeURL}pokemon/${pokemonName}`
-        );
-
+        const response = await fetch(`${pokeURL}pokemon/${pokemonName}`);
         // parse de response naar een json
         this.pokemon = await response.json();
         this.pokemonCry = new Audio(this.pokemon.cries.latest);
@@ -38,26 +43,40 @@ export default {
         console.log("error: ", error);
       }
     },
-
     async fetchSpecies() {
       try {
         const response = await fetch(this.pokemon.species.url);
         // parse de data naar json
         this.species = await response.json();
+        this.fetchEvolutionChain();
       } catch (error) {
         console.log("error ", error);
+      }
+    },
+    async fetchEvolutionChain() {
+      try {
+        const response = await fetch(this.species.evolution_chain.url);
+        this.evolutionChain = await response.json();
+        this.evolutionHandler();
+      } catch (error) {
+        console.log("error", error);
       }
     },
     async changeForm(url) {
       try {
         const response = await fetch(url);
-
         // weer ff parsen
         let formData = await response.json();
         this.currentImg = formData.sprites.front_default;
       } catch (error) {
         console.log("error", error);
       }
+    },
+    evolutionHandler() {
+      console.log("evohandler", this.evolutionChain);
+      this.evoStage1.name = this.evolutionChain.chain.species.name;
+      this.evoChain = this.evolutionChain.chain.evolves_to;
+      console.log(this.evoChain[0].species.name);
     },
   },
   computed: {
@@ -72,6 +91,9 @@ export default {
     hasVariants() {
       // computed om de template dom te houden
       return this.species?.varieties?.length > 1;
+    },
+    hasEvolutionChain() {
+      return this.evolutionChain?.chain;
     },
   },
   watch: {
@@ -98,7 +120,7 @@ export default {
     </ul>
 
     <p v-if="descriptions.length" class="description">
-      {{ descriptions[1].flavor_text }}
+      {{ descriptions[0].flavor_text }}
     </p>
     <p v-else>helaas ik weet niks</p>
 
@@ -145,12 +167,47 @@ export default {
       </ul>
     </article>
 
-    <article>
-      <h2>Moves</h2>
+    <article v-if="hasEvolutionChain" class="evolution">
+      <h2>Evoluties</h2>
+      <div class="stage1">
+        <img
+          @click="fetchPokemon(evoStage1.name)"
+          :src="currentImg"
+          class="evoImg"
+          alt=""
+        />
+        <p>{{ evoStage1.name }}</p>
+      </div>
+      <div class="stage2" v-for="stage2 in evoChain" :key="stage2.species.name">
+        <img
+          @click="fetchPokemon(stage2.species.name)"
+          :src="currentImg"
+          class="evoImg"
+          alt=""
+        />
+        <p>
+          {{ stage2.species.name }}
+        </p>
+        <div
+          class="stage3"
+          v-for="stage3 in stage2.evolves_to"
+          :key="stage3.species.name"
+        >
+          <img
+            @click="fetchPokemon(stage3.species.name)"
+            :src="currentImg"
+            class="evoImg"
+            alt=""
+          />
+          <p>
+            {{ stage3.species.name }}
+          </p>
+        </div>
+      </div>
     </article>
 
     <article>
-      <h2>Evoluties</h2>
+      <h2>Moves</h2>
     </article>
   </div>
   <div v-else>
@@ -190,6 +247,76 @@ a {
       text-align: center;
     }
   }
+
+  .evolution {
+    grid-column: 1 / -1;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    h2 {
+      grid-column: 1 /-1;
+    }
+    .stage1 {
+      grid-column: 1;
+      grid-row: 2 / 100;
+      justify-content: center;
+      display: grid;
+      align-content: center;
+      height: 100%;
+      .evoImg {
+        height: 125px;
+        width: 125px;
+      }
+      p {
+        text-align: center;
+        width: 100%;
+      }
+    }
+    .stage2 {
+      grid-column: 2;
+      display: grid;
+
+      grid-template-columns: 1fr 1fr;
+
+      p {
+        grid-column: 1;
+        text-align: center;
+        width: 100%;
+      }
+      .evoImg {
+        grid-column: 1;
+        margin: 0 auto;
+      }
+
+      .stage3 {
+        grid-column: 2;
+        grid-row: 1 / 3;
+        display: grid;
+        justify-content: center;
+
+        grid-template-columns: 1fr;
+        height: 100%;
+
+        p {
+          grid-column: 1;
+          grid-row: 3;
+        }
+        .evoImg {
+          grid-column: 1;
+        }
+      }
+    }
+    p {
+      width: 100px;
+    }
+
+    .evoImg {
+      height: 125px;
+      width: 125px;
+      grid-row: unset;
+      cursor: pointer;
+    }
+  }
+
   .pokemon-id {
     grid-column: 2;
     grid-row: 1;
